e8cdab66cae398016bd63430f5c70a47bd6f91c0
Eitan Seri-Levi <eserilev@ucsc.edu>
2017-12-24 13:30:25 -0800

    bugfix

--- src/app/layouts/secure/secure.routes.ts
+++ src/app/layouts/secure/secure.routes.ts
@@ -1,7 +1,9 @@
+import { ClientComponent } from './../../secure/client/client.component';
 import { TrainerHomeComponent } from './../../secure/trainer/trainer-home/trainer-home.component';
 import { AuthGuardService } from './../../services/auth-guard.service';
 import { Routes, RouterModule } from '@angular/router';
 
 export const SECURE_ROUTES: Routes = [
-    {path: 'trainer', component: TrainerHomeComponent, canActivate: [AuthGuardService], pathMatch: 'full'}
+    {path: 'trainer', component: TrainerHomeComponent, canActivate: [AuthGuardService], pathMatch: 'full'},
+    {path: 'client', component: ClientComponent, canActivate: [AuthGuardService], pathMatch: 'full'}
 ]
