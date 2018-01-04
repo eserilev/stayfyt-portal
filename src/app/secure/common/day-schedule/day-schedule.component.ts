3feb24c934acb0913b1c86c28175c9e46e26a74d
Eitan Seri-Levi <eserilev@ucsc.edu>
2018-01-04 00:38:55 -0800

    minor update

--- src/app/secure/common/day-schedule/day-schedule.component.ts
+++ src/app/secure/common/day-schedule/day-schedule.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { SchedulerService } from './../../../services/scheduler.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material';
 
 @Component({
   selector: 'day-schedule',
   templateUrl: './day-schedule.component.html',
-  styleUrls: ['./day-schedule.component.css']
+  styleUrls: ['./day-schedule.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DayScheduleComponent implements OnInit {
 
-  constructor() { }
+  events: string[] = [];
+
+  constructor(private schedService: SchedulerService) {
+
+    schedService.getSessions('test').subscribe(res => {
+
+      console.log(res);
+    });
+   }
 
   ngOnInit() {
+  }
+
+  myFilter = (d: Date): boolean => {
+    //if session doesnt fall on date return false
+    console.log(d);
+    const day = d.getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  }
+
+  addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+    this.events.push(`${type}: ${event.value}`);
+  }
+
+  changeDate(event: MatDatepickerInputEvent<Date>) {
+    var date = event.value;
+    //populate session list for current day
   }
 
 }
@@ -33,3 +61,6 @@ var appt =
   endTime: "10:30am",
   category: "yoga",
 }
+
+
+//mat-calendar-body-cell-content
--- src/app/services/scheduler.service.ts
+++ src/app/services/scheduler.service.ts
@@ -1,8 +1,30 @@
+
+import { User } from './../models/user.model';
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class SchedulerService {
 
-  constructor() { }
+  private baseUrl = environment.STAYFYT_API_URL + "session/"
+  constructor(private http: HttpClient) { }
+
+  getSessions(email: string): Observable<any> {
+    let params = new HttpParams();
+    params.append('email', 'new92');
+    return this.http.get(environment.STAYFYT_API_URL + "mySessions/" + email);
+  }
+
+  createSession() {
+
+  }
+
+  cancelSession() {
+
+  }
 
 }
